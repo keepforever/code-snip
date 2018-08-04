27b9dc166f18afe63060f071a8d2c775674b84ab
Brian Cilenti <briancilenti@gmail.com>
2018-08-04 15:21:15 -0700

    added rerouting for protected routes.

--- src/routes.js
+++ src/routes.js
@@ -1,72 +1,83 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { withRouter } from "react-router";
+import { connect } from "react-redux";
 // main routes
-import Home from './pages/Home';
-import AddSnip from './pages/AddSnip';
-import DataViz from './pages/DataViz';
-import SearchSnippits from './pages/SearchSnippits';
+import Home from "./pages/Home";
+import AddSnip from "./pages/AddSnip";
+import DataViz from "./pages/DataViz";
+import SearchSnippits from "./pages/SearchSnippits";
 
 const routes = [
   {
     path: "/",
     exact: true,
-    main: () => <Home />
+    main: () => <Home />,
+    protected: false
   },
   {
     path: "/add-snip",
     exact: true,
-    main: () => <AddSnip />
+    main: () => <AddSnip />,
+    protected: true
   },
   {
     path: "/data-viz",
     exact: true,
-    main: () => <DataViz />
+    main: () => <DataViz />,
+    protected: true
   },
   {
     path: "/search-snips",
     exact: true,
-    main: () => <SearchSnippits />
-  },
-  // {
-  //   path: "/about",
-  //   exact: true,
-  //   main: () => <About />
-  // },
-  // {
-  //   path: "/blog",
-  //   exact: true,
-  //   main: () => <Blog />
-  // },
-  // {
-  //   path: "/music",
-  //   exact: true,
-  //   main: () => <Music />
-  // },
-  // {
-  //   path: "/cheeks",
-  //   exact: true,
-  //   main: () => <Cheeks />
-  // }
-]
+    main: () => <SearchSnippits />,
+    protected: true
+  }
+];
 
-
-
-const Routes = ( props ) => {
-  return(
-      <Switch>
-        {routes.map((route, index) => {
+const Routes = props => {
+  console.log("routs props", props);
+  return (
+    <Switch>
+      {routes.map((route, index) => {
+        if (route.protected) {
           return (
             <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={route.main}
-              />
-            );
-        })}
-      </Switch>
-  )
+              exact
+              key={index}
+              path={route.path}
+              render={() =>
+                props.shouldShowLanding ? <Redirect to="/" /> : <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.main}
+                />
+              }
+            />
+          );
+        } else {
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.main}
+            />
+          );
+        }
+      })}
+    </Switch>
+  );
 };
 
-export default Routes;
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter.count,
+    user: state.user.userInfo,
+    snipp: state.snippit.snippits,
+    shouldShowLanding: state.landingPage.showLandingPage
+  };
+};
+
+export default withRouter(connect(mapStateToProps)(Routes));
