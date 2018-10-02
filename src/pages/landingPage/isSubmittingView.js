312f2838b11bfa4ccf64a4a70bdc91481c265293
Brian Cilenti <briancilenti@gmail.com>
2018-10-02 13:23:23 -0700

    ready to push to master... i hope

--- src/pages/landingPage/isSubmittingView.js
+++ src/pages/landingPage/isSubmittingView.js
@@ -1,37 +1,38 @@
 import React from 'react';
-import { LoadSpinContainer } from '../../components/styled/'
+//material-ui
 import CircularProgress from "@material-ui/core/CircularProgress";
-import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
-import {
-  ModalContainer,
-  WelcomeContainer,
-  HelpContainer
-} from "../../components/styled";
-import OuterSpace from "../../components/outer-space";
+//local
+import MoOutHelp from '../../components/MoOutHelp'
+
 
 const IsSubmittingView = ( props ) => {
 
   return (
-    <ModalContainer>
-      <OuterSpace>
-        <HelpContainer>
-          <LoadSpinContainer>
-            <div style={{ padding: 10, marginBottom: 10}}>
-              <Typography variant="subheading" color="secondary">
-                <strong>
-                  Note: This site is hosted on two separate free services and
-                  sometimes they can be slow or fail. If you experience a
-                  login wait time greater than six seconds, refresh the page
-                  to try again.
-                </strong>
-              </Typography>
-            </div>
-            <CircularProgress thickness={7} />;
-          </LoadSpinContainer>
-        </HelpContainer>
-      </OuterSpace>
-    </ModalContainer>
+    <MoOutHelp>
+      <div
+        style={{
+          flexDirection: "column",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: 400
+        }}
+        >
+          <div style={{ padding: 10, marginBottom: 10}}>
+            <Typography variant="subheading" color="secondary">
+              <strong>
+                Note: This site is hosted on two separate free services and
+                sometimes they can be slow or fail. If you experience a
+                login wait time greater than six seconds, refresh the page
+                to try again.
+              </strong>
+            </Typography>
+          </div>
+          <CircularProgress thickness={7} />;
+        </div>
+    </MoOutHelp>
   )
 };
 
